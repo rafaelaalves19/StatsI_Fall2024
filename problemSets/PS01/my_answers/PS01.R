a83d313fe14b7bf14c8cd670e0402d82c3840906
Rafaela Alves <62647664+rafaelaalves19@users.noreply.github.com>
2024-09-24 17:33:54 -0400

    question2 done

--- problemSets/PS01/my_answers/PS01.R
+++ problemSets/PS01/my_answers/PS01.R
@@ -8,44 +8,101 @@
 # y <− c ( 1 0 5 , 6 9 , 8 6 , 1 0 0 , 8 2 , 1 1 1 , 1 0 4 , 1 1 0 , 8 7 , 1 0 8 , 8 7 , 
 #9 0 , 9 4 , 1 1 3 , 1 1 2 , 9 8 , 8 0 , 9 7 , 9 5 , 1 1 1 , 1 1 4 , 8 9 , 9 5 , 1 2 6 , 9 8 )
 
-# 1. Find a 90% confidence interval for the average student IQ in the school.
 
+# 1. Find a 90% confidence interval for the average student IQ in the school.
 
 # data set:
 y <- c(105, 69, 86, 100, 82, 111, 104, 110, 87, 108, 87, 90, 94, 113, 112, 98,
        80, 97, 95, 111, 114, 89, 95, 126, 98)
 
-
 # mean and standard deviation
 mean_y <- mean(y)
 sd_y <- sd(y)
-
 
 # 90% confidence interval using t.test
 confidence_interval <- t.test(y, conf.level = 0.90)
 confidence_interval$conf.int
 
 
-
-
 # 2. Next, the school counselor was curious whether the average student IQ in her school
 #is higher than the average IQ score (100) among all the schools in the country.
 #Using the same sample, conduct the appropriate hypothesis test with α = 0.05.
-
 
 # data set:
 y <- c(105, 69, 86, 100, 82, 111, 104, 110, 87, 108, 87, 90, 94, 113, 112, 98,
        80, 97, 95, 111, 114, 89, 95, 126, 98)
 
-
 # one-sample t-test:
 t_test_result <- t.test(y, mu = 100, alternative = "greater", conf.level = 0.05)
 t_test_result
-
 
 # Conclusion:
 # Because the p-value is 0.7215 and it's greater than 0.05, I do not have enough evidence 
 # to reject the null hypothesis that average student IQ in the school is equal to the 
 # national average of 100 IQ score.
+
+
+#####################
+# Problem 2
+#####################
+
+
+# uploading expenditure data set
+getwd()
+#reading the file
+expenditure <- read.table("expenditure.txt", header = TRUE, sep = "\t")
+head(expenditure)
+str(expenditure)
+View(expenditure)
+
+
+
+# Scatter plot of Y vs X1
+plot(expenditure$X1, expenditure$Y, main="Y vs X1", xlab="personal income", ylab="expenditure on shelters", col="blue", pch=19)
+# There's a correlation between the per capita expenditure in shelters related to the group that has a personal income of 1.600 to 2.100 per capita
+# which doesn't make sense to me because I think the state should spend more on housing assistance with people with lower personal income
+
+
+# Scatter plot of Y vs X2
+plot(expenditure$X2, expenditure$Y, main="Y vs X2", xlab="financially insecure residents", ylab="expenditure on shelters", col="red", pch=19)
+# the spent per capita for housing assistance is higher where there are more financially insecure residents which makes sense to me because 
+# the housing assistance spending are concentrated where the demographic density are higher for financially insecure residents
+
+
+# Scatter plot of Y vs X3
+plot(expenditure$X3, expenditure$Y, main="Y vs X3", xlab="residing in urban areas", ylab="expenditure on shelters", col="green", pch=19)
+# the graph shows that spending in housing assistance is lower in rural areas and hight in urban areas
+
+
+# Scatter plot of Y vs Region
+plot(expenditure$Region, expenditure$Y, main="Y vs Region", xlab="1 = NE, 2 = NC, 3 = S, 4 = W", ylab="expenditure on shelters", col="pink", pch=19)
+# On average, the West region has the highest per capita expenditure on housing assistance.
+
+
+
+############## Reproducing Y vs X1 including one more variable Region and
+############## displaying different regions with different types of symbols and colors.
+install.packages("ggplot2")
+library(ggplot2)
+
+# Sample data
+expenditure <- data.frame(
+  Y = rnorm(100, mean = 50, sd = 10),  # Random data for Y
+  X1 = rnorm(100, mean = 30, sd = 5),  # Random data for X1
+  Region = sample(c("1.Northeast", "2.North Central", "3.South", "4.West"), 100, replace = TRUE)  # Random regions
+)
+
+
+ggplot(expenditure, aes(x = X1, y = Y, color = Region, shape = Region)) +
+  geom_point(size = 3) +  # Adjust the size of the points if needed
+  labs(title = "Y vs X1 vs Region",
+       x = "personal income per capita",
+       y = "expenditure on shelters") +
+  scale_shape_manual(values = c(16, 17, 15, 3)) +  # Specify shapes: 16 = circle, 17 = triangle, 15 = square, 3 = plus
+  theme_minimal()
+
+
+
+
 
 
