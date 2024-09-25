f173d512f5864c23a84ff08325db885102e68dbb
Rafaela Alves <62647664+rafaelaalves19@users.noreply.github.com>
2024-09-25 18:02:04 -0400

    updates_v2

--- problemSets/PS01/my_answers/PS01.R
+++ problemSets/PS01/my_answers/PS01.R
@@ -36,6 +36,7 @@ y <- c(105, 69, 86, 100, 82, 111, 104, 110, 87, 108, 87, 90, 94, 113, 112, 98,
 # one-sample t-test:
 t_test_result <- t.test(y, mu = 100, alternative = "greater", conf.level = 0.05)
 t_test_result
+#############################################>>>>>>>>>>>>>>>>>> elaborate better
 
 # Conclusion:
 # Because the p-value is 0.7215 and it's greater than 0.05, I do not have enough evidence 
